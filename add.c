0b84a61c3e7d9948da1836358cefc2d24c67c7b2
KananKarim <kenankerimli7770@gmail.com>
2024-01-11 06:04:31 -0800

    trying fixing add

--- add.c
+++ add.c
@@ -10,15 +10,15 @@ void add(stack_t **stack, unsigned int line_number)
 {
 	stack_t *temp;
 
-	if (!stack || !*stack || !(*stack)->next)
+	if (!*stack || !(*stack)->next)
 	{
 		free(opcode), free(str), free(line), fclose(stream), sfree();
 		fprintf(stderr, "L%d: can't swap, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 
-	temp = (*(*stack)).next;
-	(*(*stack)).next = (*temp).next;
-	(*(*stack)).n += (*temp).n;
+	temp = *stack;
+	*stack = (*stack)->next;
+	(*stack)->n += temp->n;
 	free(temp);
 }
