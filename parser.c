cee0aaa1b2f3e0d187c26f61298f1ea3b08757e7
pxmpkeen <pxmpkeen@gmail.com>
2024-01-11 02:45:23 -0800

    hot fix

--- parser.c
+++ parser.c
@@ -10,7 +10,7 @@ void parser(char **line, unsigned int ln)
 {
 	char *command; /* exctracted command from line */
 
-	command = strtok(*line, " \n\t");
+	command = strtok(*line, " \n\t$");
 	if (command == NULL)
 		return;
 
@@ -25,7 +25,7 @@ void parser(char **line, unsigned int ln)
 		exit(EXIT_FAILURE);
 	}
 
-	str = strtok(NULL, " \n\t");
+	str = strtok(NULL, " \n\t$");
 
 	interpreter(opcode, str, ln);
 }
